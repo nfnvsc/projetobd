68ce0fc0058d91f5196378eff4da6cef713571e3
nfnvsc <55752855+nfnvsc@users.noreply.github.com>
2020-11-16 16:15:50 +0000

    Add files via upload

---
+++ queries.sql
@@ -0,0 +1,13 @@
+/*Qual o concelho onde se fez o maior volume de vendas hoje?*/
+
+
+
+/*Qual o médico que mais prescreveu no 1º semestre de 2019 em cada região?*/
+
+
+
+/*Quais são os médicos que já prescreveram aspirina em receitas aviadas em todas as farmácias
+    do concelho de Arouca este ano? */
+
+
+/*Quais são os doentes que já fizeram análises mas ainda não aviaram prescrições este mês?*/
