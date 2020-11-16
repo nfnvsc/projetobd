b31d9e27541cbd2c448f376dc9ea16ac1480040f
Nuno <nfnvsc@hotmail.com>
2020-11-16 17:31:27 +0000

    added queries.sql

--- queries.sql
+++ queries.sql
@@ -1,13 +1,53 @@
-/*Qual o concelho onde se fez o maior volume de vendas hoje?*/
+/**
+* 1.
+* Qual o concelho onde se fez o maior volume de vendas hoje?
+*/
+
+--venda_farmacia->inst->concelho
+SELECT conselho.nome
+FROM concelho
+    NATURAL JOIN instituicao
+    NATURAL JOIN(
+        SELECT * 
+        FROM venda_farmacia
+        WHERE data_registo = GETDATE()
+    ) 
+-- tabela com todas vendas_farmacia do dia com os concelhos
+GROUP BY nome
+HAVING COUNT(*) > ALL
+    (SELECT count(*)
+    FROM )
 
 
 
-/*Qual o médico que mais prescreveu no 1º semestre de 2019 em cada região?*/
+
+
+/** 
+* 2.
+* Qual o médico que mais prescreveu no 1º semestre de 2019 em cada região?
+*/
+--prescricao->consulta->regiao
+SELECT medico, regiao.nome
+FROM(
+    SELECT *
+    FROM prescricao
+    WHERE data >= 01/01/2019 AND data <= 15/06/2019
+    NATURAL JOIN consulta
+    NATURAL JOIN regiao
+) 
+GROUP BY regiao
+HAVING COUNT(medico) > ALL --selecionar o medicos com mais consultas por regiao
 
 
 
-/*Quais são os médicos que já prescreveram aspirina em receitas aviadas em todas as farmácias
-    do concelho de Arouca este ano? */
+/*
+* 3.
+* Quais são os médicos que já prescreveram aspirina em receitas aviadas em todas as farmácias
+* do concelho de Arouca este ano? 
+*/
 
 
-/*Quais são os doentes que já fizeram análises mas ainda não aviaram prescrições este mês?*/
+/*
+* 4.
+* Quais são os doentes que já fizeram análises mas ainda não aviaram prescrições este mês?
+*/
