87093ce6e6860c4c35636cb33b766f67ac9979c0
Beatnancio <avenancio2000@hotmail.com>
2020-11-16 18:41:17 +0000

    syntax correction

--- schema.sql
+++ schema.sql
@@ -12,21 +12,18 @@ create table concelho (
     nome char(20), 
     num_habitantes int,
     primary key (num_concelho),
-    foreign key (num_regiao) references regiao
-        on delete cascade
+    foreign key (num_regiao) references regiao on delete cascade
 );
 
 create table instituicao (
     nome char(20),
     tipo char(11),
-        check (tipo in ('farmacia', 'laboratorio', 'clinica', 'hospital')), 
     num_regiao int,
     num_concelho int,
     primary key (nome),
-    foreign key (num_regiao) references regiao
-        on delete cascade,
-    foreign key (num_concelho) references concelho
-        on delete cascade
+    foreign key (num_regiao) references regiao on delete cascade,
+    foreign key (num_concelho) references concelho on delete cascade,
+    check (tipo in ('farmacia', 'laboratorio', 'clinica', 'hospital'))
 );
 
 create table medico (
@@ -39,14 +36,12 @@ create table medico (
 create table consulta (
     num_cedula int,
     num_doente int,
-    data_consulta date
-        check (datename(dw, data_consulta) not in ('Saturday', 'Sunday'),
+    data_consulta date,
     nome_instituicao char(20),
     primary key (num_cedula, num_doente, data_consulta),
-    foreign key (num_cedula) references medico
-        on delete cascade,
-    foreign key (nome_instituicao) references instituicao
-        on delete cascade
+    foreign key (num_cedula) references medico on delete cascade,
+    foreign key (nome_instituicao) references instituicao on delete cascade
+    --CHECK    check (select DATENAME(weekday, data_consulta) not in ('Saturday', 'Sunday'))
 );
 
 create table prescricao(
@@ -72,15 +67,8 @@ create table analise (
     quant int,
     inst char(20),
     primary key (num_analise),
-    foreign key (num_cedula, num_doente, data_consulta) 
-        references consulta(num_cedula, num_doente, data_consulta))
-        on delete cascade,
-    foreign key inst references instituicao
-        on delete cascade,
-    check (especialidade in 
-        (select M.especialidade
-        from medico as M
-        where num_cedula = M.num_cedula))
+    foreign key (num_cedula, num_doente, data_consulta) references consulta(num_cedula, num_doente, data_consulta) on delete cascade,
+    foreign key (inst) references instituicao on delete cascade
 );
 
 create table venda_farmacia (
@@ -100,11 +88,8 @@ create table prescricao_venda (
     data_consulta date,
     num_venda int,
     primary key (num_cedula, num_doente, data_consulta, num_venda),
-    foreign key (num_venda) references venda_farmacia
-        on delete cascade,
-    foreign key (num_cedula, num_doente, data_consulta) 
-        references consulta(num_cedula, num_doente, data_consulta)
-        on delete cascade
+    foreign key (num_venda) references venda_farmacia on delete cascade,
+    foreign key (num_cedula, num_doente, data_consulta) references consulta(num_cedula, num_doente, data_consulta) on delete cascade
 );
 
 
